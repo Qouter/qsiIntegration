6361d381c4c23412d3abed2b70499d251af3e7ff
anicolas <anicolas@PC-ANI.QUALITA.LOCAL>
2018-06-25 09:55:17 +0200

    Test

--- qsiIntegration/src/main/java/es/qual/Main.java
+++ qsiIntegration/src/main/java/es/qual/Main.java
@@ -52,7 +52,7 @@ public class Main {
 		}
 		else {
 			if(this.testLatest()) {
-				this.openURL("isTheLatest2.2");
+				this.openURL("isTheLatest2.3");
 			}
 			else {
 				this.replaceJar1();
@@ -207,7 +207,7 @@ public class Main {
 			NodeList nodes = doc.getElementsByTagName("pkver");
 			Node n = nodes.item(0);
 			String ver = n.getTextContent();
-			if(ver.equals("2.2")) {
+			if(ver.equals("2.3")) {
 				return true;
 			}
 			else {
